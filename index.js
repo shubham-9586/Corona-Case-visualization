3363c6ec9bb8a3df84f0805e0f120143a0c29d1b
shubham kesharwani <shubhamkesharwani2356@gmail.com>
2021-07-27 19:47:14 +0530

    corona case analysis

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function updateMap()
+{
+fetch("../corona cases/data.json")
+   .then(response=>response.json())
+   .then(rsp=>{
+    console.log(rsp.data);
+    rsp.data.forEach(element => {
+        latitude=element.latitude;
+        longitude=element.longitude;
+
+cases=element.infected;
+cases=cases;
+if(cases>255)
+{
+color="rgb(255,0,0)";
+}
+else
+{
+    color=`rgb(${cases},0,0)`;
+}
+        new mapboxgl.Marker({
+            draggable: false,
+            color: color
+        })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+    });
+})
+}
+const time=2000;
+setInterval(updateMap(),time)
